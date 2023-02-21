56265f76905b2e6aba008ed10ab8d64bfe06d816
brhanuhailu <hailubrhanu29@gmail.com>
2023-02-21 12:03:37 +0000

    src

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import _ from 'lodash';
+import './style.css';
+import Icon from './icon.png';
+import Data from './data.xml';
+import Notes from './data.csv';
+import printMe from './print.js';
+function component() {
+    const element = document.createElement('div');
+    const btn = document.createElement('button');
+  
+    // Lodash, now imported by this script
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    element.classList.add('hello');
+      // Add the image to our existing div.
+    const myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
+    
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+    
+    element.appendChild(btn);
+    console.log(Data);
+    console.log(Notes);
+    return element;
+  }
+  
+  document.body.appendChild(component());
