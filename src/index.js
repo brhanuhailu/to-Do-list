8e268c7fc08dbc54816e6c5dcd18d8d3f6f2d349
brhanuhailu <hailubrhanu29@gmail.com>
2023-02-22 20:33:59 +0000

    Update style.css

--- src/index.js
+++ src/index.js
@@ -1,39 +1,135 @@
+/* eslint no-alert: "error" */
+
 import './style.css';
+ class TaskToDo {
+  constructor() {
+    this.completed = false;
+  }
 
-const todoList = [
-  {
-    index: 1,
-    description: 'Trying',
-    completed: false,
-  },
-  {
-    index: 2,
-    description: 'Challenging',
-    completed: true,
-  },
-];
+  checked = (task) => {
+    task.completed = true;
+  }
 
-todoList.forEach((activity) => {
-  const { description, completed, index } = activity;
+  unchecked = (task) => {
+    task.completed = false;
+  }
+}
 
-  const cardList = document.querySelector('.card-list');
+class Tasks {
+  constructor(description, completed = false, index) {
+    this.description = description;
+    this.completed = completed;
+    this.index = index;
+  }
+}
 
-  const li = document.createElement('li');
-  li.setAttribute('id', index);
+class Todolist {
+  constructor() {
+    this.taskDtata = [];
+  }
 
-  const input = document.createElement('input');
-  input.setAttribute('type', 'checkbox');
-  input.checked = completed;
-  li.appendChild(input);
+  display = () => {
+    const taskitem = document.querySelector('.task-item');
+    if (taskitem) {
+      taskitem.innerHTML = '';
+    }
+    this.taskDtata.forEach((element) => {
+      const li = [];
+      li[element.index] = document.createElement('li');
+      li[element.index].setAttribute('id', element.index);
+      const p = [];
+      p[element.index] = document.createElement('p');
+      p[element.index].textContent = element.description;
+      p[element.index].setAttribute('id', element.index);
+      p[element.index].contentEditable = true;
+      const inputBox = [];
+      inputBox[element.index] = document.createElement('input');
+      inputBox[element.index].setAttribute('type', 'checkbox');
+      inputBox[element.index].classList.add('checkbox');
+      inputBox[element.index].setAttribute('id', element.index);
+      const button = [];
+      button[element.index] = document.createElement('button');
+      button[element.index].setAttribute('id', element.index);
+      button[element.index].innerHTML = '<i class="fa-solid fa-ellipsis-vertical icon"></i>';
+      li[element.index].append(inputBox[element.index], p[element.index], button[element.index]);
+      taskitem.append(li[element.index]);
 
-  const p = document.createElement('p');
-  p.setAttribute('class', 'description');
-  p.textContent = description;
-  li.appendChild(p);
+      p[element.index].addEventListener('click', (e) => {
+        e.target.nextSibling.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+        e.target.nextSibling.style.cursor = 'pointer';
 
-  const i = document.createElement('i');
-  i.setAttribute('class', 'fa-solid fa-ellipsis-vertical');
-  li.appendChild(i);
+        e.target.nextSibling.addEventListener('click', () => {
+          li[element.index].remove();
+          this.removetask(element.index);
+        });
+      });
 
-  cardList.appendChild(li);
+      li[element.index].addEventListener('mouseleave', (e) => {
+        button[element.index].innerHTML = '<i class="fa-solid fa-ellipsis-vertical"></i>';
+        this.updatetask(e.target.id, e.target.innerText);
+      });
+    });
+  }
+
+  addtask = (description, completed, index) => {
+    const newtask = new Tasks(description, completed, index);
+    this.taskDtata.push(newtask);
+    if (typeof window !== 'undefined') {
+      localStorage.setItem('LOCALLISTDB', JSON.stringify(this.taskDtata));
+    }
+  }
+
+  removetask(item) {
+    const key = item;
+    if (this.taskDtata.length === 1) {
+      this.taskDtata = [];
+    } else {
+      this.taskDtata.splice(key, 1);
+    }
+    this.taskDtata.forEach((element, index) => {
+      element.index = index;
+    });
+    if (typeof window !== 'undefined') {
+      localStorage.setItem('LOCALLISTDB', JSON.stringify(this.taskDtata));
+      this.display();
+    }
+  }
+
+  updatetask(item, description) {
+    if (this.taskDtata[item].index === Number(item)) {
+      this.taskDtata[item].description = description;
+    }
+    this.taskDtata.forEach((element, index) => {
+      element.index = index;
+    });
+    if (typeof window !== 'undefined') {
+      localStorage.setItem('LOCALLISTDB', JSON.stringify(this.taskDtata));
+      this.display();
+    }
+  }
+}
+
+let index = 0;
+const completed = false;
+const taskentry = new Todolist();
+const addnewtask = document.querySelector('.addbtn');
+const inputelement = document.querySelector('.input-element');
+addnewtask.addEventListener('click', () => {
+  if (inputelement.value === '') {
+    addnewtask.setCustomValidity('This is required field!');
+  } else {
+    taskentry.addtask(inputelement.value, completed, index);
+    taskentry.display();
+    inputelement.value = '';
+    index += 1;
+  }
 });
+
+window.onload = () => {
+  taskentry.taskDtata = JSON.parse(localStorage.getItem('LOCALLISTDB' || '[]'));
+  if (taskentry.taskDtata === null) {
+    taskentry.taskDtata = [];
+    return;
+  }
+  taskentry.display();
+};
