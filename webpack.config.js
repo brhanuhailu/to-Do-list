41b78b11bf25c92402fae92a5ca92929d3e52975
brhanuhailu <hailubrhanu29@gmail.com>
2023-02-22 09:32:25 +0000

    Update package

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-  // ...
+  entry:  './src/index.js',
   mode: 'development',
-  entry: {
-    index: './src/index.js',
-    print: './src/print.js',
-  },
-  devtool: 'inline-source-map',
   devServer: {
     static: './dist',
   },
@@ -15,40 +10,25 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
-      title: 'Development',
+      title: 'To do List',
     }),
   ],
   output: {
-    filename: '[name].bundle.js',
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-    publicPath: '/',
   },
-  optimization: {
-    runtimeChunk: 'single',
-  },
+
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(csv|tsv)$/i,
-        use: ['csv-loader'],
-      },
-      {
-        test: /\.xml$/i,
-        use: ['xml-loader'],
-      },
+      
     ],
+  },
+  optimization: {
+    // // runtimeChunk: 'single',
   },
 };
